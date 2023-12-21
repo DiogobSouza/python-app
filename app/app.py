ba89543e374c47a3f08226f2af030ea52dd51847
diogo.souza18 <diogo.souza18@outlook.com>
2023-12-21 11:05:48 -0300

    Aplicação do projeto

--- app/app.py
+++ app/app.py
@@ -1,128 +1,107 @@
-from flask import Flask, render_template, request, redirect, url_for
-from prometheus_flask_exporter import PrometheusMetrics
+from flask import Flask, render_template, request, jsonify, redirect, url_for
+from time import sleep
 import psycopg2
 import os
 import names
 
-def create_app():
-    app = Flask(__name__)
-    return app
-
 app = Flask(__name__)
-metrics = PrometheusMetrics(app)
+
 
 ## Database Configuration
 host = os.getenv("DB_HOST")
 database = os.getenv("DB_NAME")
 user = os.getenv("DB_USER")
 password = os.getenv("DB_PASSWD")
-port = os.getenv("DB_PORT")
 
-connection = psycopg2.connect(
+conn = psycopg2.connect(
             host=host,
             database=database,
             user=user,
-            password=password,
-            port=port
+            password=password
         )
 
 @app.route('/', methods=['GET','POST'])
 def index():
-    '''
-    Método index da aplicação.
-    '''
     if request.method == 'POST':
         message = request.form.get("message")
         app.logger.info(message)
         save(message)
     return render_template('index.html', data=read())
 
+
 @app.route('/healthcheck', methods=['GET'])
 def health():
-    '''
-    Método de Health Check da aplicação.
-    '''
     try:
-        connection = psycopg2.connect(
+        conn = psycopg2.connect(
             host=host,
             database=database,
             user=user,
             password=password,
-            port=port,
             connect_timeout=1
         )
-        connection.close()
+        conn.close()
         return '{ status: OK }'
     except:
         return False
+    
 
 @app.route('/limpar', methods=['GET'])
 def limpa():
-    '''
-    Método para limpar input da aplicação.
-    '''
     if request.method == 'GET':
-        connection = psycopg2.connect(
+        conn = psycopg2.connect(
             host=host,
             database=database,
             user=user,
-            password=password,
-            port=port
+            password=password
         )
 
-        db = connection.cursor()
-        db.execute("DELETE from messagesdev")
-        connection.commit()
+        db = conn.cursor()
+        db.execute("DELETE from messages")
+        conn.commit()
         db.close()
-        connection.close()
+        conn.close()
     return redirect(url_for('index'))
 
 
 @app.route('/popular', methods=['GET','POST'])
 def popular():
-    '''
-    Método para popular com dados genéricos a aplicação.
-    '''
     for i in range(1000):
         message = names.get_full_name()
         save(message)
     return redirect(url_for('index'))
        
+
 def save(message):
-    '''
-    Método para salvar informações da aplicação.
-    '''
-    connection = psycopg2.connect(
+    conn = psycopg2.connect(
             host=host,
             database=database,
             user=user,
-            password=password,
-            port=port
+            password=password
         )
-    cur = connection.cursor()
-    sql = "INSERT INTO messagesdev(message) VALUES(%s);"
+    cur = conn.cursor()
+    sql = "INSERT INTO messages(message) VALUES(%s);"
     cur.execute(sql, (message,))
-    connection.commit()
+    conn.commit()
     cur.close()
-    connection.close()
+    conn.close()
+
 
 def read():
-    '''
-    Método para ler informações do BD da aplicação.
-    '''
-    connection = psycopg2.connect(
+    conn = psycopg2.connect(
            host=host,
            database=database,
            user=user,
-           password=password,
-           port=port
+           password=password
        )
-    db = connection.cursor()
-    db.execute("SELECT * FROM messagesdev")
+    db = conn.cursor()
+    db.execute("SELECT * FROM messages")
     data = db.fetchall()
     db.close()
-    connection.close()
+    conn.close()
     return data
 
+
+
 if __name__ == "__main__":
-    app.run(debug=False, host="0.0.0.0", port=8501)
+    app.run(debug=False, host="0.0.0.0", port=8000)
+
